69e45871ecdd444dca05fe14fb49a62bed2d6784
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-12-02 16:37:28 +0700

    Added game categories

---
+++ constants/categories.ts
@@ -0,0 +1,50 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Game Boy Advance',
+    slug: 'game-boy-advance',
+    image: 'images/category/game-boy-advance.png'
+  },
+  {
+    id: 2,
+    name: 'NES',
+    slug: 'nes',
+    image: 'images/category/nes.png'
+  },
+  {
+    id: 3,
+    name: 'Game Boy',
+    slug: 'game-boy',
+    image: 'images/category/game-boy.png'
+  },
+  {
+    id: 4,
+    name: 'SNES',
+    slug: 'snes',
+    image: 'images/category/snes.png'
+  },
+  {
+    id: 5,
+    name: 'Nintendo 64',
+    slug: 'nintendo-64',
+    image: 'images/category/nintendo-64.png'
+  },
+  {
+    id: 6,
+    name: 'Master System',
+    slug: 'master-system',
+    image: 'images/category/master-system.png'
+  },
+  {
+    id: 7,
+    name: 'Mega Drive',
+    slug: 'mega-drive',
+    image: 'images/category/mega-drive.png'
+  },
+  {
+    id: 8,
+    name: 'Game Gear',
+    slug: 'game-gear',
+    image: 'images/category/game-gear.png'
+  }
+];
