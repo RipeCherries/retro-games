fe267f9fce8aa716329b6fa4f9e5380c5f46e023
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-12-03 15:24:48 +0700

    Added games for seeding

---
+++ constants/games.ts
@@ -0,0 +1,113 @@
+export const games = [
+  {
+    id: 1,
+    name: 'Super Mario Advance',
+    slug: 'super-mario-advance',
+    description:
+      'Super Mario Advance — это переиздание классической игры из серии Super Mario, ' +
+      'выпущенное для портативной консоли Game Boy Advance в 2001 году. Игра сочетает в себе ' +
+      'элементы оригинальных игр Super Mario Bros. 2 (также известной как Super Mario USA в Японии) ' +
+      'и обновленных визуальных и звуковых характеристик.',
+    image: 'images/game/gba/super-mario-advance.png',
+    gameUrl: 'roms/gba/super-mario-advance.zip',
+    core: 'gba',
+    categoryId: 1
+  },
+  {
+    id: 2,
+    name: 'Duck Tales',
+    slug: 'duck-tales',
+    description:
+      'DuckTales — культовая игра для NES, выпущенная в 1989 году компанией Capcom. ' +
+      'Созданная по мотивам популярного одноимённого мультсериала, она сочетает в себе динамичный ' +
+      'платформенный геймплей, уникальную механику и очаровательный дух приключений.',
+    image: 'images/game/nes/duck-tales.png',
+    gameUrl: 'roms/nes/duck-tales.zip',
+    core: 'nes',
+    categoryId: 2
+  },
+  {
+    id: 3,
+    name: 'Shantae',
+    slug: 'shantae',
+    description:
+      'Shantae — это приключенческая платформенная игра, выпущенная для Game Boy Color ' +
+      'в 2002 году. Разработанная студией WayForward, она стала одной из последних игр для этой ' +
+      'платформы и завоевала культовый статус благодаря инновационному геймплею, яркой графике и ' +
+      'запоминающимся персонажам.',
+    image: 'images/game/gb/shantae.png',
+    gameUrl: 'roms/gb/shantae.zip',
+    core: 'gb',
+    categoryId: 3
+  },
+  {
+    id: 4,
+    name: 'Aladdin',
+    slug: 'aladdin',
+    description:
+      'Aladdin для Super Nintendo Entertainment System (SNES) — это классическая ' +
+      'приключенческая платформенная игра, выпущенная в 1993 году компанией Capcom. ' +
+      'Она основана на популярном диснеевском мультфильме Aladdin и отличается уникальным стилем, ' +
+      'проработанным геймплеем и атмосферным саундтреком.',
+    image: 'images/game/snes/aladdin.png',
+    gameUrl: 'roms/snes/aladdin.zip',
+    core: 'snes',
+    categoryId: 4
+  },
+  {
+    id: 5,
+    name: 'Super Smash Bros',
+    slug: 'super-smash-bros',
+    description:
+      'Super Smash Bros — это культовый файтинг, выпущенный в 1999 году компанией Nintendo. ' +
+      'Игра объединила персонажей из разных игровых франшиз компании, предложив уникальный подход ' +
+      'к жанру: вместо традиционных полос здоровья и комбо здесь используется система выбивания ' +
+      'соперников с арены.',
+    image: 'images/game/n64/super-smash-bros.png',
+    gameUrl: 'roms/n64/super-smash-bros.7z',
+    core: 'n64',
+    categoryId: 5
+  },
+  {
+    id: 6,
+    name: 'Sonic the Hedgehog',
+    slug: 'sonic-the-hedgehog',
+    description:
+      'Sonic the Hedgehog — это 2D-платформер, выпущенный в 1991 году. ' +
+      'Эта версия игры, разработанная студией Ancient, является адаптацией оригинальной игры ' +
+      'Sonic the Hedgehog для более доступной 8-битной платформы. Несмотря на технические ограничения, ' +
+      'она сохранила дух оригинала и предложила уникальный взгляд на дебют знаменитого синего ежа.',
+    image: 'images/game/segaMS/sonic-the-hedgehog.png',
+    gameUrl: 'roms/segaMS/sonic-the-hedgehog.7z',
+    core: 'segaMS',
+    categoryId: 6
+  },
+  {
+    id: 7,
+    name: 'Comix Zone',
+    slug: 'comix-zone',
+    description:
+      'Comix Zone — это уникальный 2D-битемап, выпущенный в 1995 году, ' +
+      'который выделяется своим оригинальным дизайном, вдохновлённым комиксами. ' +
+      'Игра разработана компанией Sega Technical Institute и считается одной из самых стильных и ' +
+      'инновационных игр для своей эпохи.',
+    image: 'images/game/segaMS/comix-zone.png',
+    gameUrl: 'roms/segaMS/comix-zone.zip',
+    core: 'segaMD',
+    categoryId: 7
+  },
+  {
+    id: 8,
+    name: 'Battletoads',
+    slug: 'battletoads',
+    description:
+      'Battletoads — это порт легендарной игры, впервые выпущенной в 1991 году для NES. ' +
+      'Версия для Game Gear, разработанная компанией Rare и изданная Tradewest, вышла в 1993 году. ' +
+      'Она сохраняет фирменный хардкорный геймплей оригинала, добавляя свои особенности ' +
+      'благодаря особенностям платформы.',
+    image: 'images/game/segaGG/battletoads.png',
+    gameUrl: 'roms/segaGG/battletoads.7z',
+    core: 'segaGG',
+    categoryId: 8
+  }
+];
