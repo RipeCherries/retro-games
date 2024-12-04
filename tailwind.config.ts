cbd47885f62d7b2866e8aec38f63045f791b2609
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-12-04 18:27:26 +0700

    Added custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#071D2C',
+        main: '#031322',
+        accent: '#446171',
+        'accent-secondary': '#0A2737'
+      }
+    }
   },
   plugins: []
 } satisfies Config;
